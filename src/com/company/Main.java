36850ed993645affc62eee6cfc7b6239732943d4
KurbakoAnastasiia <kurbako.anastasia@gmail.com>
2021-01-26 19:56:01 -0500

    Solar System ready

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.sun = 1;
+        FactorX.moon = 3;
+        FactorX.planets = 2;
+
     }
 }
